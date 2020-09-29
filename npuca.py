d164ec3cc888b41a5254282cbe54c5b875359640
Alex Beng <599267065@qq.com>
2020-09-29 15:35:19 +0800

    user log in/out and register

--- npuca.py
+++ npuca.py
@@ -1,1 +1,9 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+
+# 设置 flask shell 上下文\
+# 让 flask shell 里面db有东西
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
